7591919b2a3c06585196405045238d38659c9a7d
yiryeong0720 <yiryeong0720@gmail.com>
2022-03-03 15:02:24 +0900

    add question get (index, detail) funtion
    Signed-off-by: yiryeong0720 <yiryeong0720@gmail.com>

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 
+# 네임스페이스
+app_name = 'pybo'
+
 urlpatterns = [
-	path("", views.index)
+	path("", views.index, name='index'),
+	path("<int:question_id>/", views.detail, name='detail'),
 ]
