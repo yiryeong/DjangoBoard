c6bc2b4f5378d4cc29f6ce0345005b60de382881
yiryeong0720 <yiryeong0720@gmail.com>
2022-03-07 12:06:12 +0900

    add signup feature
    Signed-off-by: yiryeong0720 <yiryeong0720@gmail.com>

--- common/urls.py
+++ common/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 
 app_name = 'common'
@@ -9,4 +10,5 @@ urlpatterns = [
     # LoginView는 registration/login.html 를 기본으로 호출한다. 아래처런 설정하여 common/login.html 호출하도록 변경
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
--- common/views.py
+++ common/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from common.forms import UserForm
 
-# Create your views here.
+
+def signup(request):
+    """
+    회원가입
+    :param request:
+    :return:
+    """
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            # authenticate, login 함수는 django.contrib.auth  패키지에 있는 함수로 사용자 인증과 로그인을 담당한다.
+            # 회원가입이 완료 된 이후에 자동으로 로그인 되도록 authenticate, login 함수 사용
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserForm()
+    return render(request, 'common/signup.html', {'form': form})
