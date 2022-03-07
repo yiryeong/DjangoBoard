3924c61a523e822793cd41ca476bcbf4de38fae8
yiryeong0720 <yiryeong0720@gmail.com>
2022-03-07 13:03:30 +0900

    add modify feature
    Signed-off-by: yiryeong0720 <yiryeong0720@gmail.com>

--- pybo/models.py
+++ pybo/models.py
@@ -7,6 +7,9 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    # modify_date에 Null 허용
+    # blank=True는 form.is_valid()를 통한 입력 폰 데이터 검사 시 값이 없어도 된다는 의미
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
@@ -17,3 +20,4 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
--- pybo/views.py
+++ pybo/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
@@ -79,5 +80,34 @@ def question_create(request):
 	else:
 		# method == 'GET'
 		form = QuestionForm()
+	context = {'form': form}
+	return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+	"""
+	pybo 질문 수정
+	:param request:
+	:param question_id: 질문 레코드 아이디
+	:return:
+	"""
+	question = get_object_or_404(Question, pk=question_id)
+	if request.user != question.author:
+		messages.error(request, '수정권한이 없습니다.')
+		return redirect('pybo:detail', question_id=question.id)
+
+	if request.method == 'POST':
+		# question을 기본값으로 하여 화면으로 전달받은 입력값들을 덮어써서 QuestionForm을 생성하라는 의미
+		form = QuestionForm(request.POST, instance=question)
+		if form.is_valid():
+			question = form.save(commit=False)
+			question.author = request.user
+			question.modify_date = timezone.now()
+			question.save()
+			return redirect('pybo:detail', question_id=question.id)
+	else:
+		# instance=question 지정하면 기존 값을 폼에 채울 수 있다.
+		form = QuestionForm(instance=question)
 	context = {'form': form}
 	return render(request, 'pybo/question_form.html', context)
